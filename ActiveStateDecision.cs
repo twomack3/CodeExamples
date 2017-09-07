3a9e805f05e616f8fc3f10cdb2c0601af8be69b7
Travis Womack <twomack4@student.alamo.edu>
2017-09-07 01:56:23 -0500

    Update ActiveStateDecision.cs

--- ActiveStateDecision.cs
+++ ActiveStateDecision.cs
@@ -7,7 +7,6 @@ public class ActiveStateDecision : Decision
 {
     public override bool Decide(PlayerStateController controller)
     {
-
         return controller.playerAITransform.gameObject.activeSelf;
     }
 }
