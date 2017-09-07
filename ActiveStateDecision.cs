6bf4368cca04bf467d9114580bae077bfb8e1fcd
Travis Womack <twomack4@student.alamo.edu>
2017-09-07 01:56:05 -0500

    Update ActiveStateDecision.cs

--- ActiveStateDecision.cs
+++ ActiveStateDecision.cs
@@ -7,10 +7,7 @@ public class ActiveStateDecision : Decision
 {
     public override bool Decide(PlayerStateController controller)
     {
-        //if (controller.stateTimeElapsed > controller.debugPrintTime)
-        //{
-        //    //Debug.Log("ASD " + controller +" "+ controller.playerAITransform.gameObject.activeSelf);
-        //}
+
         return controller.playerAITransform.gameObject.activeSelf;
     }
 }
